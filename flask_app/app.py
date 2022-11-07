e76d8ca688987e44ca00d44d27f418550b5e4235
jamsun9907 <jamsun9907@gmail.com>
2022-11-07 20:14:41 +0900

    heroeo

--- flask_app/app.py
+++ flask_app/app.py
@@ -13,7 +13,8 @@ model = None
 @app.route('/', methods=['GET', 'POST']) 
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return '<h3>Hi, heroku!</h3>'
+        # return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
